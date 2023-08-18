038af2cfd75eab9bd03efd4157793765a61aa6dc
sumitra <sumitra.mohanty@bipros.com>
2023-08-18 17:28:14 +0530

    added svg icons

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// IMPORTS //
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
+// SVG //
+import Calender from "./assets/icons/calender.svg";
+import Check from "./assets/icons/check.svg";
+
+/** App */
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  // View starts here 
+	return (
+		<View style={styles.container}>
+			<Text>SVG Icons Examples</Text>
+			<Calender style={{margin:10}} height={20} width={20} />
+      <Check  style={{margin:10}} height={30} width={30}/>
+
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
