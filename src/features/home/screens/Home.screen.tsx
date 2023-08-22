734b95027336d7f3c5221df09299c79631c5b5c9
sumitra <sumitra.mohanty@bipros.com>
2023-08-22 20:44:09 +0530

    updated code for textinput and added colors as needed from css

--- src/features/home/screens/Home.screen.tsx
+++ src/features/home/screens/Home.screen.tsx
@@ -1,11 +1,12 @@
 // IMPORTS //
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { Alert, View, TouchableOpacity, Text } from "react-native";
 
 // STYLES //
 
 // COMPONENTS //
 import Button from "../../../components/common-components/Button";
+import TextInputBox from "../../../components/common-components/TextInputBox";
 
 // SVG //
 import Close from "../../../../assets/icons/close.svg";
@@ -17,12 +18,33 @@ import Close from "../../../../assets/icons/close.svg";
 /** Home screen component */
 const HomeScreen: React.FC = () => {
 	// States
+	const [userNameInput, setUserNameInput] = useState<string>(""); // Initialize a state variable to hold the user's input for a username
+	const [isErrorUserName, setIsErrorUserName] = useState<boolean>(false); // Initialize a state variable to track whether there's an error with the username input
+	const [errorMessage, setErrorMessage] = useState<string>(""); // Initialize a state variable to store an error message related to the username input
 
 	// Helper functions
 	/** On click Button */
 	const handleButtonPress = () => {
 		// Handle button press logic here
 		console.log("Button Pressed");
+	};
+
+	/** For TextInput (Validates the user name input based on custom rules.) */
+	const handleValidateUserName = () => {
+		// Check if the user name is empty or too short
+		if (userNameInput.trim() === "" || userNameInput.length < 3) {
+			setIsErrorUserName(true);
+			setErrorMessage(
+				userNameInput.trim() === ""
+					? "Username cannot be empty"
+					: "Username must be at least 3 characters long"
+			);
+		} else {
+			// Validation succeeded
+			Alert.alert("User name is correct"); // Added for testing purpose
+			setIsErrorUserName(false);
+			setErrorMessage("");
+		}
 	};
 
 	// View starts
@@ -37,6 +59,37 @@ const HomeScreen: React.FC = () => {
 				size="big"
 				mode="block"
 			></Button>
+
+			{/** View for TextInputBox */}
+			<View style={{ padding: 10 }}>
+				<TextInputBox
+					label="User Name"
+					placeholder="User Name"
+					value={userNameInput}
+					errorMessage={errorMessage}
+					isError={isErrorUserName}
+					onClear={() => {
+						setUserNameInput("");
+						setIsErrorUserName(false);
+					}}
+					onChangeText={(text) => {
+						setUserNameInput(text);
+						setIsErrorUserName(false);
+						setErrorMessage("");
+					}}
+					multiline={false}
+					style={{}}
+				/>
+
+				{/* Button to Trigger Validation , added to test TextInput */}
+				{/* When clicked, this button triggers the handleValidateUserName function */}
+				<TouchableOpacity
+					style={{ alignSelf: "center" }}
+					onPress={handleValidateUserName}
+				>
+					<Text>Click to Check</Text>
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 };
