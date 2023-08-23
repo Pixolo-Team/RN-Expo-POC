125e529f3ed11d86ebbc7dbff37790c969f58f7c
sumitra <sumitra.mohanty@bipros.com>
2023-08-23 17:59:57 +0530

    added loader

--- src/features/home/screens/Home.screen.tsx
+++ src/features/home/screens/Home.screen.tsx
@@ -1,10 +1,11 @@
 // IMPORTS //
 import React, { useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 
 // STYLES //
 
 // COMPONENTS //
+import Loader from "../../../components/common-components/Loader";
 
 // SVG //
 
@@ -17,10 +18,18 @@ const HomeScreen: React.FC = () => {
 	// Define Contexts
 
 	// Define States
+	const [loading, setLoading] = useState(false);
 
 	// Define Refs
 
 	// Helper Functions
+	/**  Simulate a delay and show the loader (added for testing purpose) */
+	const startLoading = () => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, 3000); // Simulated loading time
+	};
 
 	// Use Effect and Focus Effect
 
@@ -28,6 +37,15 @@ const HomeScreen: React.FC = () => {
 	return (
 		<View style={styles.container}>
 			<Text>Home Screen Works</Text>
+
+			{/** Loader for testing */}
+			<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+				<TouchableOpacity onPress={startLoading} >
+					<Text>Start Loading</Text>
+				</TouchableOpacity>
+				<Loader visible={loading} text="Loading..." />
+			</View>
+
 		</View>
 	);
 };
