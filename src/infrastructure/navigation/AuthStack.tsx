4c2ff9fc09f4b46a1cd1ce0f58d67f4a93632c5f
Pratham3006 <2020.pratham.shetty@ves.ac.in>
2024-01-23 21:20:11 +0530

    'Navigation'

--- src/infrastructure/navigation/AuthStack.tsx
+++ src/infrastructure/navigation/AuthStack.tsx
@@ -3,18 +3,28 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 // SCREENS //
 import HomeScreen from "../../features/home/screens/Home.screen";
+import About from "../../features/about/screens/About.screen";
+import { useNavigation } from "@react-navigation/native";
 
 const AuthStack = createStackNavigator();
 
+
 /** Create the Navigation for the Auth Stack */
 const AuthStackScreen: React.FC = () => {
+	const nav2=useNavigation();
 	return (
-		<AuthStack.Navigator initialRouteName={"Home"}>
+		<AuthStack.Navigator initialRouteName={"Home"} >
 			<AuthStack.Screen
 				name="Home"
 				component={HomeScreen}
-				options={{ headerShown: false }}
+				options={{ headerShown: false}}
 			/>
+			<AuthStack.Screen
+				name="About"
+				component={About}
+				options={{ headerShown: false}}
+			/>
+			
 		</AuthStack.Navigator>
 	);
 };
