6d15e875a0671de54e9e251870cb144ab82f507d
Aliya9702 <aliya.pixolo@gmail.com>
2024-01-30 20:48:06 +0530

    remove dropdown file

--- src/features/home/screens/Home.screen.tsx
+++ src/features/home/screens/Home.screen.tsx
@@ -1,11 +1,10 @@
 // IMPORTS //
-import React, { useState } from "react";
+import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
 // STYLES //
 
 // COMPONENTS //
-import DropDown from "../../../components/common-components/DropDown";
 
 // SVG //
 
@@ -18,15 +17,6 @@ const HomeScreen: React.FC = () => {
 	// Define Contexts
 
 	// Define States
-	const [selectedValueForDropdown, setSelectedValueForDropdown] =
-		useState<string>("");
-
-	/** Array Variable for Dropdown Items List */
-	const dummyOptions = [
-		{ label: "Option 1", value: "option1" },
-		{ label: "Option 2", value: "option2" },
-		{ label: "Option 3", value: "option3" },
-	];
 
 	// Define Refs
 
@@ -38,22 +28,6 @@ const HomeScreen: React.FC = () => {
 	return (
 		<View style={styles.container}>
 			<Text>Home Screen Works</Text>
-
-			<DropDown
-				label="Expense Type"
-				style={{ margin: 20 }}
-				dropdownItems={dummyOptions}
-				selectedValue={selectedValueForDropdown}
-				onItemChange={(text, label) => {
-					setSelectedValueForDropdown(text);
-					// If you want to set the label as well, you can do it here
-					// label && setSelectedLabelForDropdown(label);
-				}}
-				placeholder={{
-					label: "Select an option",
-					value: "", // Specify the value for the placeholder
-				}}
-			/>
 		</View>
 	);
 };
