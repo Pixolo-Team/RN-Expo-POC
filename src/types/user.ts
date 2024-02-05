219eb358985b19e0061634c5734e8f8aec3f422f
Aliya9702 <aliya.pixolo@gmail.com>
2024-02-05 20:27:01 +0530

    update user data type

--- src/types/user.ts
+++ src/types/user.ts
@@ -7,21 +7,15 @@ interface Device {
 	platform?: string;
 	status?: 0 | 1; // 0?: Logged Out, 1?: Logged In
 }
-interface ContactInfo {
-	email?: string;
-	phone_number?: string;
-}
 
 type UserData = {
 	_id?: string;
-	bio?: string;
-	contact_info?: ContactInfo;
 	devices?: Device[];
 	dob?: Date;
 	email?: string;
+	phone_number: string;
 	first_name?: string;
 	last_name?: string;
-	login_type?: 1 | 2; // 1?: username
 	profile_photo?: string; // Relative path
 	password?: string;
 	token: string;
@@ -41,4 +35,4 @@ type LoginApiData = {
 	token: string;
 };
 
-export { ContactInfo, Device, UserCoreData, UserData, LoginApiData };
+export { Device, UserCoreData, UserData, LoginApiData };
