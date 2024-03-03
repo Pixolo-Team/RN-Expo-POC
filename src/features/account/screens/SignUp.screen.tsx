122d26767b6bb33c2f2ada50a1985de673cf4d43
Adarsh Anchan <adarsh.anchan@ting.in>
2024-03-04 01:21:35 +0530

    signup validation outsourced

--- src/features/account/screens/SignUp.screen.tsx
+++ src/features/account/screens/SignUp.screen.tsx
@@ -28,13 +28,7 @@ import {
 } from "../../../services/analytics.service";
 
 // UTILS //
-import {
-	validateConfirmPassword,
-	validateEmail,
-	validateName,
-	validatePassword,
-	validatePhoneNumber,
-} from "../../../utils/validation.util";
+import { validateSignUpFormInputs } from "../../../utils/signUpFormInputValidation";
 
 /** Sign up screen component */
 const SignUpScreen: React.FC = () => {
@@ -76,80 +70,12 @@ const SignUpScreen: React.FC = () => {
 		});
 	};
 
-	/** Function to checks the input field validation of Sign Up form */
-	const validateSignUpFormInputs = () => {
-		let valid = true;
-
-		// Validates the Name input field
-		if (formInputs.first_name === "" || !validateName(formInputs.first_name)) {
-			setFormErrors((pastErrors) => ({
-				...pastErrors,
-				first_name_error: "You need to enter a valid First name",
-			}));
-			valid = false;
-		}
-
-		// Validates the Last Name input field
-		if (formInputs.last_name === "" || !validateName(formInputs.last_name)) {
-			setFormErrors((pastErrors) => ({
-				...pastErrors,
-				last_name_error: "You need to enter a valid Last name",
-			}));
-			valid = false;
-		}
-
-		// Validates the Email input field
-		if (!formInputs.email || !validateEmail(formInputs.email)) {
-			setFormErrors((pastErrors) => ({
-				...pastErrors,
-				email_error: "You need to enter a valid Email address",
-			}));
-			valid = false;
-		}
-
-		// Validates the Phone input field
-		if (
-			formInputs.phone_number === "" ||
-			!validatePhoneNumber(formInputs.phone_number)
-		) {
-			setFormErrors((pastErrors) => ({
-				...pastErrors,
-				phone_number_error: "You need to enter a 10 Digit Phone number",
-			}));
-			valid = false;
-		}
-
-		// Validates the Password input field
-		if (formInputs.password === "" || !validatePassword(formInputs.password)) {
-			setFormErrors((pastErrors) => ({
-				...pastErrors,
-				password_error:
-					"Password must contain atleast 8 characters with atleast one Special character, one Uppercase letter and one Number.",
-			}));
-			valid = false;
-		}
-
-		// Validates the Confirm Password input field
-		if (
-			formInputs.confirm_password === "" ||
-			!validateConfirmPassword(formInputs.password, formInputs.confirm_password)
-		) {
-			setFormErrors((pastErrors) => ({
-				...pastErrors,
-				confirm_password_error: "Your Password is not matching",
-			}));
-			valid = false;
-		}
-
-		return valid;
-	};
-
 	/** Submit the Sign Up Form */
 	const handleSignUp = async () => {
 		// Reset the errors
 		resetErrors();
 		// Check Form validity
-		if (validateSignUpFormInputs()) {
+		if (validateSignUpFormInputs(formInputs, setFormErrors)) {
 			try {
 				// Send the data to the endpoint
 				const signUpResponse = await createUserRequest(formInputs);
